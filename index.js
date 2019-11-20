bcac4389c6c98f59ad9d87ed9f29b3fbda1733a2
wizzyx-subzy <57808473+wizzyx-subzy@users.noreply.github.com>
2019-11-20 15:26:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const mySearch = document.querySelector('#search');
+const container = document.querySelector('.container')
+
+mySearch.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+document.querySelector('#search');
+}
+
+mySearch.Value = ' ';
+console.log(mySearch.Value);
+ const tc = new TimeMax{
+ 	tc.fromTo(container, 1, {height:"0%"}, {height:"20%" ease: power2.easeInOut})
+ 	tc.fromTo(container 1.5, {width:"100%"}, {width:"50%"} ease: power2.easeInOut)
+ }
